3d51e1c6820357f7ab6ec79ffc3853ee828dcfb0
q5089877 <q5089877@gmail.com>
2021-12-09 11:42:28 +0800

    Add project files.

---
+++ BLE_TEST_DOSE/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BLE_TEST_DOSE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
